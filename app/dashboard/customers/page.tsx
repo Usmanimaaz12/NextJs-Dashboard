ce4f8733964afc22c219da796baa1dd4997fe8cd
Maaz Usmani <maaz.usmani@squareboat.com>
2024-08-12 16:00:17 +0530

    ADD : metadata and default credentials

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <p>Customers Page</p>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Coming Soon...</h1>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import { inter } from './ui/fonts';
 
 export const experimental_ppr = true;
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
